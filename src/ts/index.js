65eed70d9c8caa50b00bfae5d2d96cf6f92502e6
johnedisc <christopher.johnedis@gmail.com>
2023-07-14 12:52:07 -0700

    add stylesheet

---
+++ src/ts/index.js
@@ -0,0 +1,9 @@
+var printNumber = function () {
+    var div = document.createElement('div');
+    div.innerHTML = '6';
+    div.classList;
+    document.querySelector('#container');
+};
+window.addEventListener('load', function (event) {
+    console.log('this is the main event: ', event);
+});
