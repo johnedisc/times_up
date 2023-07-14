414119820f6aa8889d880d09caaa5126e2aa71de
johnedisc <christopher.johnedis@gmail.com>
2023-07-13 23:24:47 -0700

    add entry files

---
+++ src/ts/index.ts
@@ -0,0 +1,11 @@
+const printNumber = ():void => {
+
+  const div: HTMLElement = document.createElement('div');
+  div.innerHTML = '6';
+  div.classList
+  document.querySelector('#container');
+};
+
+window.addEventListener('load', (event) => {
+  console.log('this is the main event: ', event);
+});
