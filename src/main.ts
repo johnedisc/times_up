245e64a2d46a34cf7395677b84ee25eb8f2a909d
johnedisc <christopher.johnedis@gmail.com>
2023-07-18 17:30:38 -0700

    move files for vite format

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,18 @@ document.querySelector<HTMLDivElement>('#container')!.innerHTML = `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
+
+const entry = ():void => {
+  const buildStartScreen = ():void => {
+    const h1: HTMLElement = document.createElement('h1');
+    h1.innerHTML = 'OPEN';
+    h1.classList.add('start-screen');
+    document.querySelector('#container')?.appendChild(h1);
+  };
+
+  window.addEventListener('load', (event) => {
+    //  screen.orientation.lock("landscape");
+    buildStartScreen();
+  });
+};
+
