c2733f87de1b1115be140dfd8eb3cf9d665c5a65
johnedisc <christopher.johnedis@gmail.com>
2023-08-18 10:36:30 -0700

    update interfaces and Store
    create a proxy to emit events when data changes

--- src/services/Store.ts
+++ src/services/Store.ts
@@ -1,13 +1,22 @@
+import { IStore } from "../utilities/interfaces.ts";
 
-export const Store = {
+export const Store: IStore = {
   user: null,
   container: null,
-  currentSequence: null,
-  currentIndex: null
+  currentProgram: null,
+  currentIndex: 0
 }
 
 export const storeProxy = new Proxy(Store, {
   set(target, property, value) {
     target[property] = value;
+    if (property === 'currentProgram') {
+      window.dispatchEvent(new Event('programchanged'));
+    } else if (property === 'currentIndex') {
+      window.dispatchEvent(new Event('indexchanged'));
+    } else if (property === 'user') {
+      window.dispatchEvent(new Event('userchanged'));
+    }
+    return true;
   }
 });
--- src/utilities/interfaces.ts
+++ src/utilities/interfaces.ts
@@ -8,6 +8,13 @@ export interface IUser {
   timerList: ITimerList[]
 }
 
+export interface IStore {
+  user: IUser | null,
+  container: HTMLElement | null,
+  currentProgram: ITimerList | null,
+  currentIndex: number
+}
+
 declare global {
   interface Window {
     _timesUpApp: any,
