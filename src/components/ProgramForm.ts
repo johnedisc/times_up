c843d7ad10f08f51279b3a87ac37ab6724b72c75
johnedisc <christopher.johnedis@gmail.com>
2023-08-22 13:06:30 -0700

    add bindings to form

--- src/components/ProgramForm.ts
+++ src/components/ProgramForm.ts
@@ -23,21 +23,41 @@ export class ProgramForm extends HTMLElement {
         <button type='button'>go</button>
       </form>
     `;
-    this.querySelector('form').classList.add('flex-down', 'start-screen');
+
+    const programForm = this.querySelector('form');
+    programForm.classList.add('flex-down', 'start-screen');
+    console.log(programForm?.elements);
+
+    this.setFormBindings(programForm);
+//    window.addEventListener('inputchanged', (event) => {
+//      console.log('hi');
+//      console.log(this.#newProgram);
+//    });
   }
 
-  setFormBindings(form) {
+  setFormBindings(form: HTMLFormElement) {
     //set dbl data binding
+    console.log('set bindings');
     this.#newProgram = new Proxy(this.#newProgram, {
       set(target, property, value) {
         try {
-          Boolean(target[property]) ? target[property] = value : new Error('property doesn\'t exist');
           form.elements[property].value = value;
+//          console.log('hi');
+//          window.dispatchEvent(new Event('inputchanged'));
         } catch(error) {
           console.log(error);
         }
       },
     })
+    for (let i = 0; i < form.elements.length; i++) {
+      let el = form.elements[i];
+      console.log(el);
+      el.addEventListener('change', (event) => {
+        this.#newProgram[el.name] = el.value;
+        console.log(event.target);
+        console.log(el.value);
+      });
+    };
   }
 }
 
