748ab734ce640e9cc2acd66c2bd499d985bb7cfc
johnedisc <christopher.johnedis@gmail.com>
2023-08-31 09:38:58 -0700

    empty git index of dist folders

--- client/dist/services/LoadData.js
+++
@@ -1,5 +0,0 @@
-import { _timesUpApp } from "../main.js";
-import { UserDataAPI } from "./UserDataAPI.js";
-export const LoadData = async () => {
-    _timesUpApp.store.user = await UserDataAPI.fetchUser();
-};
