bc436b1e211bb3864dd3f361cad41bef1b1e1bfe
Chris Johnedis <christopher.johnedis@gmail.com>
2023-09-21 11:56:16 -0700

    update client to send request to server

--- client/src/services/UserDataAPI.ts
+++ client/src/services/UserDataAPI.ts
@@ -1,8 +1,9 @@
 export const UserDataAPI = {
-  url: "/src/data/UserData.json",
+  url: "localhost:5432",
   fetchUser: async () => {
     try {
       const result = await fetch(UserDataAPI.url);
+      console.log('this is the fetch\n', result);
       return await result.json();
     } catch (error) {
       console.error(error);
