eb1b16b231db4c6fbd50d9b5dd956acc57fc6858
Chris Johnedis <christopher.johnedis@gmail.com>
2023-10-05 12:38:07 -0700

    update tables for groups, programs, intervals

--- server/queries.sql
+++ server/queries.sql
@@ -4,20 +4,21 @@ group_name VARCHAR(255) NOT NULL,
 owner_id INT REFERENCES user_info(id) ON DELETE CASCADE
 );
 
-CREATE TABLE interval_program_id (
+CREATE TABLE interval_program (
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-user_id INT REFERENCES user_info(user_id) ON DELETE CASCADE,
-group_id INT REFERENCES groups(group_id) ON DELETE CASCADE
+user_id INT REFERENCES user_info(id) ON DELETE CASCADE,
+group_id INT REFERENCES groups(id) ON DELETE CASCADE
 );
 
 CREATE TABLE interval (
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-time_seconds SMALLINT NOT NULL CHECK (
-time_seconds > 0
-AND time_seconds < 32000
+time_seconds INT NOT NULL CHECK (
+  time_seconds > 0
 ),
 interval_name VARCHAR(255) NOT NULL,
-sequence_number SMALLINT UNIQUE NOT NULL,
+sequence_number SMALLINT UNIQUE NOT NULL CHECK (
+  sequence_number > 0
+),
 interval_program_id INT REFERENCES interval_programs(interval_program_id) ON DELETE CASCADE
 );
 
