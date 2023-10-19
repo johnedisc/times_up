284ff42245056cba4e12c8c3b02a00b7f51cba55
Chris Johnedis <christopher.johnedis@gmail.com>
2023-10-19 15:25:38 -0700

    add form elements to program form

--- client/src/components/ProgramForm.ts
+++ client/src/components/ProgramForm.ts
@@ -1,9 +1,12 @@
 import { _timesUpApp } from "../main.js";
+import { clearSelf } from "../utilities/utilities.js";
 
 export class ProgramForm extends HTMLElement {
 
-  #newProgram: Record<string | symbol, string> = {
-    name: ""
+  #newProgram: Record<string | symbol, string | string[]> = {
+    name: "",
+    groups: "",
+    intervals: []
   }
 //  #newListItem = {
 //    name: "",
@@ -16,13 +19,19 @@ export class ProgramForm extends HTMLElement {
   }
 
 
-  importGroupNames():void {
+  importGroupNames():string {
     console.log('import groups');
-    console.log(_timesUpApp.store);
+    console.log(_timesUpApp.store.user.groups.group_name);
+    let html: string = '';
+    for (let i = 0; i < _timesUpApp.store.user.groups.length; i++) {
+      const name: string = _timesUpApp.store.user.groups[i].group_name;
+      html += `<option value='${name}'>${name}</option>`;
+    }
+    return html;
   }
 
   formName():void {
-    const groups = this.importGroupNames();
+    const groups: string = this.importGroupNames();
     this.innerHTML = `
       <form>
         <label for='name'>
@@ -34,12 +43,54 @@ export class ProgramForm extends HTMLElement {
           group name
         </label>
         <select name='groups' id='group-select'>
+          <option value=''>/*select a group*/</option>
+          ${groups}
         </select>
 
-        <button type='submit'>go</button>
+        <button type='submit'>send</button>
       </form>
     `;
 
+    const programForm = document.querySelector('form');
+    programForm?.classList.add('flex-down', 'start-screen');
+
+    if (programForm) this.setFormBindings(programForm);
+  }
+
+  intervals():void {
+//    clearSelf(this);
+    const intervalElements = `
+        <label for='name'>
+          program name
+        </label>
+        <input name='name' type='text' required />
+
+        <label for='length'>
+          length in seconds
+        </label>
+        <input name='length' type='number' required />
+        `;
+    this.innerHTML = `
+      <h1 class='h4'>${this.#newProgram.name}</h1>
+      <form id='intervals'>
+        <label for='name'>
+          program name
+        </label>
+        <input name='name' type='text' required />
+
+        <label for='length'>
+          length in seconds
+        </label>
+        <input name='length' type='number' required />
+
+        <button type='submit'>send</button>
+      </form>
+      <p id='add-interval'>add another interval</p>
+    `;
+
+    document.getElementById('add-interval')?.addEventListener('click', () => {
+      document.querySelector('button')?.before(intervalElements);
+    });
     const programForm = document.querySelector('form');
     programForm?.classList.add('flex-down', 'start-screen');
 
@@ -54,8 +105,7 @@ export class ProgramForm extends HTMLElement {
 
     form.addEventListener('submit', (event) => {
       event.preventDefault();
-      console.log(`${this.#newProgram.name} submitted`);
-//      this.#newProgram.name = '';
+      this.intervals();
 
     });
 
@@ -72,9 +122,9 @@ export class ProgramForm extends HTMLElement {
     })
     for (let i = 0; i < form.elements.length; i++) {
       let el = form.elements[i] as HTMLInputElement;
-//      console.log(el.name);
+      console.log(el.name);
       el.addEventListener('change', () => {
-//        console.log(el, el.value);
+        console.log(el, el.value);
         let tempVal = el.value;
         this.#newProgram[el.name] = tempVal;
       });
